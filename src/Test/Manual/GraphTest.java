03eba4bf111d1acf1a817d54ddfed33a6108ce99
Luminatione <01171749@pw.edu.pl>
2022-04-19 13:01:56 +0200

    Manual testing

--- src/Test/Manual/GraphTest.java
+++ src/Test/Manual/GraphTest.java
@@ -2,13 +2,15 @@ package Test.Manual;
 
 import JimpProject2.graph.Graph;
 import JimpProject2.graph.graphFactory.GraphFileLoader;
+import JimpProject2.graph.graphFactory.RandomGraphGenerator;
 
 public class GraphTest {
     public static void main(String[] args)
     {
-        try {
-            System.out.println(System.getProperty("user.dir"));
+        try
+        {
             Graph graph = new GraphFileLoader("test").create();
+            graph = new RandomGraphGenerator(3, 5, 0.1, 3.0).create();
         }
         catch (Exception e)
         {

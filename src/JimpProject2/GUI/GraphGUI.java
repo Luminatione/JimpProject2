0aecd8cc3368a928eaeb13fbaae53ddfbbd5e84f
Luminatione <01171749@pw.edu.pl>
2022-05-22 12:28:00 +0200

    to remove

--- src/JimpProject2/GUI/GraphGUI.java
+++ src/JimpProject2/GUI/GraphGUI.java
@@ -1,7 +1,5 @@
 package JimpProject2.GUI;
 
-import JimpProject2.algorithm.dijkstra.Dijkstra;
-import JimpProject2.algorithm.dijkstra.DijkstraResult;
 import JimpProject2.graph.Edge;
 import JimpProject2.graph.Graph;
 
@@ -11,11 +9,11 @@ import java.util.ArrayList;
 
 public class GraphGUI extends GraphGUIWrapper
 {
-    static int rootIndex = -1, endIndex = -1;
-    static int mouseClicks = 0;
+    int rootIndex, endIndex;
+    int mouseClicks = 0;
     int nodeSize = 10;
     int padding = (int) (nodeSize * 0.8f);
-    public static Graph graph;
+    public Graph graph;
     double minWeight = Double.MAX_VALUE;
     double maxWeight = 0;
 
@@ -102,22 +100,8 @@ public class GraphGUI extends GraphGUIWrapper
         if (mouseClicks % 2 == 0)//??
             rootIndex = nodeY * graph.getColumns() + nodeX;
         else
-        {
             endIndex = nodeY * graph.getColumns() + nodeX;
-        }
+        DijkstraController dijksraController = new DijkstraController(rootIndex, endIndex, graph);
         mouseClicks++;
-    }
-
-    public static String beginClicked()
-    {
-        if(rootIndex == -1)
-            return "Please select a root and destination";
-        if(endIndex == -1)
-            return "Please select a destination";
-        else
-        {
-            DijkstraResult result = new Dijkstra(graph, rootIndex).compute();
-            return "Length of path between " + rootIndex + " and " + endIndex + " is: " + result.pathLengths.get(endIndex);
-        }
     }
 }

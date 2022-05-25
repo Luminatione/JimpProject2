5679e5955aee03a1ac84d67f25c1a8c8f2fa1950
Patryk Łazowski <jakosziewski@gmail.com>
2022-05-25 07:20:36 +0200

    improved tuple hashing

--- src/JimpProject2/utility/Tuple.java
+++ src/JimpProject2/utility/Tuple.java
@@ -2,20 +2,17 @@ package JimpProject2.utility;
 
 import java.util.Objects;
 
-public class Tuple implements Comparable<Tuple>
-{
+public class Tuple implements Comparable<Tuple> {
     public Integer a;
     public Integer b;
 
-    public Tuple(Integer a, Integer b)
-    {
+    public Tuple(Integer a, Integer b) {
         this.a = a;
         this.b = b;
     }
 
     @Override
-    public boolean equals(Object o)
-    {
+    public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Tuple tuple = (Tuple) o;
@@ -23,14 +20,12 @@ public class Tuple implements Comparable<Tuple>
     }
 
     @Override
-    public int hashCode()
-    {
-        return Objects.hash(a, b) + Objects.hash(b, a) + a % (b+1) + b % (a+1);
+    public int hashCode() {
+        return (Objects.hash(a, b) + Objects.hash(b, a)) + (a %(b+1) +  b % (a+1));
     }
 
     @Override
-    public int compareTo(Tuple o)
-    {
+    public int compareTo(Tuple o) {
         return hashCode() - o.hashCode();
     }
 }
